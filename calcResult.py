065d5cd2cb1bae3686620e6c7b853514270440a6
Josh Fowler <objectiveattitude@gmail.com>
2023-10-11 14:52:41 -0400

    restructure and touchup of basic logic, functionality completed

--- calcResult.py
+++ calcResult.py
@@ -1,12 +1,27 @@
+from tkinter import *
+import tkinter as tk
 
-# Second window
-def addGameLength(arg):
-    # gameLength = gameLengthConfirm
-    gameLength = arg
+
+def setGameLength(Window, gameTime, sessionAmmount):
+    print(Window)
+    gameLength = int(gameTime)
+    # Store game length from entry passed in via arg
+    # print(type(gameLength))
+    # Verify Storage
+    
     print(str(gameLength) + " hours")
 
-def addSessionCount(arg):
-    # NEED TO DISABLE BUTTON UNTIL GAME LENGTH IS CONFIRMED
-    # gameLength = gameLengthConfirm
-    sessionCount = arg
-    print(str(sessionCount) + " sessions over " + str(arg) + " hours")
+    addSessionCount(Window, int(sessionAmmount), int(gameTime))
+
+
+def addSessionCount(Window, sessions, gameTime):
+    newWindow = Toplevel(Window)
+    newWindow.title("Result")
+    # Create Result Window
+    
+
+    estimatedTimePerSession = gameTime/sessions
+    estimatedTimePerSession = round(estimatedTimePerSession * 2.0) / 2.0
+    print(estimatedTimePerSession)
+    print("Your estimated commitement is " + str(estimatedTimePerSession) + "hrs per session, over " + str(sessions) + " sessions.")
+    tk.Label(newWindow, text="Your estimated commitment is roughly " + str(estimatedTimePerSession) + " hours per session, over " + str(sessions) + " sessions.").grid(row=1, column=0)
