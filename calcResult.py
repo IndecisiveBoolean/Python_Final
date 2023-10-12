f16ea3eee016942e2af114ecf019c98644f3f22d
Josh Fowler <objectiveattitude@gmail.com>
2023-10-11 20:59:45 -0400

    images added and additional styling begun

--- calcResult.py
+++ calcResult.py
@@ -3,25 +3,16 @@ import tkinter as tk
 
 
 def setGameLength(Window, gameTime, sessionAmmount):
-    print(Window)
-    gameLength = int(gameTime)
-    # Store game length from entry passed in via arg
-    # print(type(gameLength))
-    # Verify Storage
-    
-    print(str(gameLength) + " hours")
-
     addSessionCount(Window, int(sessionAmmount), int(gameTime))
-
 
 def addSessionCount(Window, sessions, gameTime):
     newWindow = Toplevel(Window)
     newWindow.title("Result")
     # Create Result Window
     
-
     estimatedTimePerSession = gameTime/sessions
     estimatedTimePerSession = round(estimatedTimePerSession * 2.0) / 2.0
-    print(estimatedTimePerSession)
-    print("Your estimated commitement is " + str(estimatedTimePerSession) + "hrs per session, over " + str(sessions) + " sessions.")
+    # Calculate expected length of sessions in order to beat the game within the desired time.
+
     tk.Label(newWindow, text="Your estimated commitment is roughly " + str(estimatedTimePerSession) + " hours per session," + "\n" +" over " + str(sessions) + " sessions.").grid(row=1, column=0)
+    # Render a new label containing the results of the above calculation and build within second window using grid.
--- main.py
+++ main.py
@@ -3,15 +3,41 @@ from tkinter import *
 from PIL import ImageTk, Image  
 import calcResult
 
+
+
 mainWindow = tk.Tk()
 mainWindow.title("How Long To Beat")
-# mainWindow.geometry("300x300")
+mainWindow.geometry("500x281")
+mainWindow.configure()
+
+
 
 frame = Frame(mainWindow)
 frame.pack()
 
 
-titleLabel = tk.Label(frame, text = "Calculate how long it will take you to beat a game!").grid(column=1, row=1)
+# Image is opened and resized appropriately
+background = Image.open("./images/birdman_oshi-no-ko.png")
+background = background.resize((500, 281))
+
+# image converter to PhotoImage for better compatibility with Tkinter
+piyo_bg = ImageTk.PhotoImage(background)
+
+# Frame for bg is created
+    # Allows bg to be beneath main elements/widgets
+frame = Frame(mainWindow, relief="raised", borderwidth=2)
+# frame is inserted via pack and set to fill/expand
+frame.pack(fill=BOTH, expand=YES)
+# frame.pack_propagate(False)
+
+# Label is created to contain bg image
+    # Label inserted into window via place.
+bg_label = Label(frame, image=piyo_bg)
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+# header is created
+titleLabel = tk.Label(frame, text = "Calculate how long it will take you to beat a game!", bg="black", fg="white").grid(column=1, row=1)
 
 
 
@@ -19,22 +45,26 @@ titleLabel = tk.Label(frame, text = "Calculate how long it will take you to beat
 ########### ENTRY ### LABEL ### ENTRY ############
 
 gameLengthEntryLabel = tk.Label(frame, text = "Enter length of game?")
-gameLengthEntryLabel.grid(column=1, row=2)
+gameLengthEntryLabel.grid(column=1, row=2, pady=(40, 0))
 gameLengthEntry = tk.Entry(frame, text = "Input the length of your game")
-gameLengthEntry.grid(column=1, row=3)
+gameLengthEntry.grid(column=1, row=3, pady=(10, 0))
 
-sessionEntryLabel = tk.Label(frame, text = "How many sessions?").grid(column=1, row=4)
+sessionEntryLabel = tk.Label(frame, text = "How many sessions?").grid(column=1, row=4, pady=(10, 0))
 sessionCountEntry = tk.Entry(frame, text = "Input your estimated sessions")
-sessionCountEntry.grid(column=1, row=5)
+sessionCountEntry.grid(column=1, row=5, pady=(10, 0))
 
 ########### ENTRY ### LABEL ### ENTRY ############
 
 
-########### BUTTONS ### BUTTONS ### BUTTONS ############
 
-calculateMyTime = Button(frame, text = "Confirm Game Length", command=lambda: calcResult.setGameLength(mainWindow, gameLengthEntry.get(), sessionCountEntry.get())).grid(column=1, row=6)
 
 ########### BUTTONS ### BUTTONS ### BUTTONS ############
+
+calculateMyTime = Button(frame, text = "Confirm Game Length", command=lambda: calcResult.setGameLength(mainWindow, gameLengthEntry.get(), sessionCountEntry.get())).grid(column=1, row=6, pady=(30, 0))
+
+########### BUTTONS ### BUTTONS ### BUTTONS ############
+
+
 
 
 mainWindow.mainloop()
